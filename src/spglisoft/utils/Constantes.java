e4a302474298ee06a7706c48bef237dc3b29aa86
camilo <camiloesan@gmail.com>
2023-12-27 23:47:54 -0600

    script actualizado y cambio de credenciales

--- src/spglisoft/utils/Constantes.java
+++ src/spglisoft/utils/Constantes.java
@@ -10,14 +10,11 @@ package spglisoft.utils;
  */
 public class Constantes {
     public static final String DRIVER = "com.mysql.jdbc.Driver";
-    public static final String NOMBRE_BD = "SPGLISOFTX";
+    public static final String NOMBRE_BD = "SPGLISOFT";
     public static final String HOSTNAME = "localhost";
     public static final String PUERTO = "3306";
     public static final String USUARIO = "root";
-    public static final String PASSWORD = "ciliegi03";
+    public static final String PASSWORD = "apple";
     public static final String USUARIO_DESARROLLADOR = "desarrollador";
     public static final String USUARIO_REPRESENTANTE = "representante_proyecto";
-    public static int ESTADO_SOLICITUD_PENDIENTE = 1;
-    public static int ESTADO_SOLICITUD_EN_PROCESO = 2;
-    public static int ESTADO_SOLICITUD_CONCLUIDA= 3;
 }
